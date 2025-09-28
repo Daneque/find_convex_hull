b560ed0e971e6b4e61da4bedf52a98180376845e
Данила Ильин <ilyin.danill2016@yandex.ru>
2025-09-28 22:37:54 +0300

    Fix a tangents search, start work with merge convex hull

--- Chazelle.cpp
+++ Chazelle.cpp
@@ -1,11 +1,14 @@
+#include "Chazelle.h"
 #include "FileUtils.h"
 #include "FindConv.h"
-#include <fstream>
-#include <sstream>
-#include <iostream>
 #include <vector>
 #include <string>
 #include "Point.h"
+#include "Tools.h"
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
 
 size_t getLeftmostPoint(std::vector<Point>& hull) {
     size_t idx = 0;
@@ -34,6 +37,11 @@ std::pair<size_t, size_t> getUpperTangent(std::vector<Point>& left_hull, std::ve
     size_t L = left_hull.size();
     size_t R = right_hull.size();
 
+    if(L == 1 && R == 1) {return {0, 0};}
+
+    bool not_one_left = L > 1 ? true : false;
+    bool not_one_right = R > 1 ? true : false;
+
     bool flag_left = true;
     bool flag_right = true;
 
@@ -41,7 +49,7 @@ std::pair<size_t, size_t> getUpperTangent(std::vector<Point>& left_hull, std::ve
 
         // проверяем точку в левой ВО
         size_t next_left = (U_L + 1) % L;
-        if (rotate(right_hull[U_R], left_hull[U_L], left_hull[next_left]) > 0) {
+        if (rotate(left_hull[U_L], right_hull[U_R], left_hull[next_left]) > 0 && not_one_left && flag_left) {
             U_L = next_left;
         } else {
             flag_left = false;
@@ -49,7 +57,8 @@ std::pair<size_t, size_t> getUpperTangent(std::vector<Point>& left_hull, std::ve
 
         // проверяем точку в правой ВО
         size_t prev_right = (U_R + R - 1) % R;
-        if (rotate(left_hull[U_L], right_hull[U_R], right_hull[prev_right]) < 0) {
+        
+        if (rotate(left_hull[U_L], right_hull[U_R], right_hull[prev_right]) > 0 && not_one_right && flag_right) {
             U_R = prev_right;
         } else {
             flag_right = false;
@@ -60,11 +69,16 @@ std::pair<size_t, size_t> getUpperTangent(std::vector<Point>& left_hull, std::ve
 }
 
 std::pair<size_t, size_t> getLowerTangent(std::vector<Point>& left_hull, std::vector<Point>& right_hull) {
-    size_t U_L = getRightmostPoint(left_hull);
-    size_t U_R = getLeftmostPoint(right_hull);
+    size_t L_L = getRightmostPoint(left_hull);
+    size_t L_R = getLeftmostPoint(right_hull);
 
     size_t L = left_hull.size();
     size_t R = right_hull.size();
+
+    if(L == 1 && R == 1) {return {0, 0};}
+
+    bool not_one_left = L > 1 ? true : false;
+    bool not_one_right = R > 1 ? true : false;
 
     bool flag_left = true;
     bool flag_right = true;
@@ -72,21 +86,74 @@ std::pair<size_t, size_t> getLowerTangent(std::vector<Point>& left_hull, std::ve
     while (flag_left || flag_right) {
 
         // проверяем точку в левой ВО
-        size_t next_left = (U_L + L - 1) % L;
-        if (rotate(right_hull[U_R], left_hull[U_L], left_hull[next_left]) < 0) {
-            U_L = next_left;
+        size_t next_left = (L_L + L - 1) % L;
+        if (rotate(left_hull[L_L], right_hull[L_R], left_hull[next_left]) < 0 && not_one_left && flag_left) {
+            L_L = next_left;
         } else {
             flag_left = false;
         }
 
         // проверяем точку в правой ВО
-        size_t prev_right = (U_R + 1) % R;
-        if (rotate(left_hull[U_L], right_hull[U_R], right_hull[prev_right]) > 0) {
-            U_R = prev_right;
+        size_t prev_right = (L_R + 1) % R;
+        if (rotate(left_hull[L_L], right_hull[L_R], right_hull[prev_right]) < 0 && not_one_right && flag_right) {
+            L_R = prev_right;
         } else {
             flag_right = false;
         }
     }
 
-    return {U_L, U_R};
+    return {L_L, L_R};
+}
+
+
+std::vector<std::vector<Point>> splitPoints(const std::vector<Point>& points) {
+    std::vector<std::vector<Point>> result;
+
+    // базовый случай
+    if (points.size() < 4) {
+        result.push_back(points);
+        return result;
+    }
+
+    size_t mid = points.size() / 2;
+
+    std::vector<Point> left(points.begin(), points.begin() + mid);
+    std::vector<Point> right(points.begin() + mid, points.end());
+
+
+    auto leftParts = splitPoints(left);
+    auto rightParts = splitPoints(right);
+
+
+    result.insert(result.end(), leftParts.begin(), leftParts.end());
+    result.insert(result.end(), rightParts.begin(), rightParts.end());
+
+    return result;
+}
+
+
+void MergeConvHull(std::vector<Point>& chl, std::vector<Point>& chr) {
+
+    std::pair<size_t, size_t> upper_tangent = getUpperTangent(chl, chr);
+    std::pair<size_t, size_t> lower_tangent = getLowerTangent(chl, chr);
+
+    //left hull 
+
+    size_t length_lpart_union = chl.size() - (upper_tangent.first - lower_tangent.first - 1);
+
+    if (length_lpart_union == 0) {
+        // вся левая ВО вошла в объединение
+    } else {
+        // лишь часть вошла, и длина этой части подсчитана
+    }
+
+    //right hull
+
+    size_t length_rpart_union = upper_tangent.second - lower_tangent.second - 1;
+
+    if(length_rpart_union == 0) {
+        // вся правая ВО вошла в объединени
+    } else {
+        // лишь часть и длина известна
+    }
 }
--- Chazelle.h
+++ Chazelle.h
@@ -6,3 +6,5 @@ size_t getLeftmostPoint(std::vector<Point>& hull);
 size_t getRightmostPoint(std::vector<Point>& hull);
 std::pair<size_t, size_t> getUpperTangent(std::vector<Point>& left_hull, std::vector<Point>& right_hull);
 std::pair<size_t, size_t> getLowerTangent(std::vector<Point>& left_hull, std::vector<Point>& right_hull);
+std::vector<std::vector<Point>> splitPoints(const std::vector<Point>& points);
+void MergeConvHull(std::vector<Point>& chl, std::vector<Point>& chr);
